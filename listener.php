7420cdc5589ed4ae62ac53d5ecbfbd450815099d
Siddharth <Siddharth.patel39@gmail.com>
2021-07-28 15:43:48 +0100

    created curl to handle the the FTP and HTTP req's

--- listener.php
+++ listener.php
@@ -3,4 +3,13 @@ if ($_SERVER['REQUEST_METHOD'!='POST']) {
     header(string:'Loction: index.php');
     exit();
 }
+$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, 'https://ipnpb.sandbox.paypal.com/cgi-bin/webscr');
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, "cmd=_notify-validate&" . http_build_query($_POST));
+	$response = curl_exec($ch);
+	curl_close($ch);
 <?>
