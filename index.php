09dcbcada57238ec3a4c55f2c018891054761bb3
Siddharth <Siddharth.patel39@gmail.com>
2021-07-28 12:26:34 +0100

    Added normal page

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pricing Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    </head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-offset-3">
+                <div class="card">
+                    <div class="card-header">
+                        <h3>50 Euro</h3>
+                    </div>
+                    <div class="card-block">
+                        <div calss="card-title">
+                            Payment notification
+                        </div>
+                        <ul class="list-group">
+                            <li class="list-group-items"> feature 1</li>
+                            <li class="list-group-items"> feature 2</li>
+                            <li class="list-group-items"> feature 3</li>
+                            <li class="list-group-items"> feature 4</li>
+                            <li class="list-group-items"> feature 5</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
