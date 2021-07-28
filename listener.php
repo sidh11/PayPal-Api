575b5ecc9a6d5a5c045ba2b324bb69ec1c3a7ffa
Siddharth <Siddharth.patel39@gmail.com>
2021-07-28 19:05:36 +0100

    mail

--- listener.php
+++ listener.php
@@ -38,8 +38,8 @@
 				$mail->isSMTP();                                            // Send using SMTP
 				$mail->Host       = ' smtp.gmail.com';                    // Set the SMTP server to send through
 				$mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-				$mail->Username   = 'nottysid@gmail.com';                     // SMTP username
-				$mail->Password   = 'wgnkzqevgcfuspya';                               // SMTP password
+				$mail->Username   = 'bhupendrabanothe@gmail.com';                     // SMTP username
+				$mail->Password   = 'pegqzsurprcbpffy';                               // SMTP password
 				$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 				$mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 			
